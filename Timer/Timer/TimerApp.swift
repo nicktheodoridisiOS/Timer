7c40af91957dd3e5eed7aad2ac098a7ee46cd131
Nick Theodoridis <122683142+nicktheodoridisiOS@users.noreply.github.com>
2023-02-11 18:07:42 +0200

    Create the project

---
+++ Timer/Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Nick Theodoridis on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
